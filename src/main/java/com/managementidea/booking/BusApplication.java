5ffaead125efb1e8ca9c6f87c613b9f8ea1ead49
Akhil <akhilpgvr@gmail.com>
2024-06-26 01:44:36 +0530

    added skeleton

---
+++ src/main/java/com/managementidea/booking/BusApplication.java
@@ -0,0 +1,13 @@
+package com.managementidea.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusApplication.class, args);
+	}
+
+}
