33cdeaf93650b38370a6d87a85cd01b2873eaf08
Akhil <akhilpgvr@gmail.com>
2024-06-26 23:25:57 +0530

    added feignclient

--- src/main/java/com/managementidea/booking/BookingApplication.java
+++ src/main/java/com/managementidea/booking/BookingApplication.java
@@ -2,8 +2,10 @@ package com.managementidea.booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class BookingApplication {
 
 	public static void main(String[] args) {
